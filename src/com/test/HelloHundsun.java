0d9bc2369557a9be809c362ccb73eb605be1349c
MrTianGiraffe <476987830@qq.com>
2017-04-17 14:31:38 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloHundsun.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test;
+
+/*** <p>Title:HelloHundsun </p>
+* <p>Description: </p>
+* <p>Company: </p> 
+* @author: tianzj
+* @date 2017年4月17日下午1:42:37
+*/
+public class HelloHundsun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("Hello Hundsun");
+	}
+
+}
